6842b284f320c21368afd53282f51c2ca9c2e64c
fun-cheer <114402588+fun-cheer@users.noreply.github.com>
2024-02-28 14:17:45 +0800

    Update simple_tokenize.cpp with token encode added

--- simple_tokenize.cpp
+++ simple_tokenize.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     int vocab_size;
@@ -45,18 +46,73 @@ void load_vocab(vocab_info *vinfo) {
     fclose(f);
 }
 
+void free_vocab(vocab_info *vinfo) {
+    for (int i = 0; i < vinfo->vocab_size; i++) {
+        free(vinfo->vocab[i]);
+    }
+
+    free(vinfo->vocab);
+}
+
+int str2token(char *str, vocab_info *vinfo) {
+    for (int i = 0; i < vinfo->vocab_size; i++) {
+        if (strcmp(str, vinfo->vocab[i]) == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+void token_encode(vocab_info *vinfo, const char *str, int *tokens, int *tokens_num) {
+    char buf[5];
+    int len = 0;
+    *tokens_num = 0;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if ((str[i] & 0xC0) != 0x80) {
+            len = 0;
+        }
+
+        buf[len++] = str[i];
+        buf[len] = '\0';
+
+        if ((str[i + 1] & 0xC0) == 0x80 && len < 4) {
+            continue;
+        }
+
+        int id = str2token(buf, vinfo);
+        if (id < 0) {
+            printf("content not found %s\n", buf);
+            return;
+        }
+
+        tokens[(*tokens_num)++] = id;
+        len = 0;
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Usage:\n\t%s sentence\n", argv[0]);
         return -1;
     }
 
+    const char *str = argv[1];
+    int tokens_num = 0;
+    int *tokens = (int *)malloc((strlen(str) + 1) * sizeof(int));
+    printf("[1] original sentence is: [%s]\n", str);
+
     vocab_info vinfo;
     load_vocab(&vinfo);
+    token_encode(&vinfo, str, tokens, &tokens_num);
 
-    for (int i = 0; i < vinfo.vocab_size; i++) {
-        printf("id %d, %s\n", i, vinfo.vocab[i]);
+    printf("[2] tokenize result is:\n");
+    for (int i = 0; i < tokens_num; i++) {
+        printf("\tid %d, %d\n", i, tokens[i]);
     }
+
+    free_vocab(&vinfo);
 
     return 0;
 }
